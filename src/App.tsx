100db7bb34d053bdd87abe73edd9540df8a1a50f
Tsvetelin <kitanskitsvetelin@gmail.com>
2023-04-17 13:04:00 +0300

    Initial commit with ready contact form

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+
+import { useTranslation } from 'react-i18next';
+
+import DrawerAppBar from './components/Header/Header';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+
+
+function App() {
+    const { t } = useTranslation();
+
+    return (
+        <div>
+            <DrawerAppBar/>
+           <Routes>
+                <Route path='/Home' element={<Home/>}/>
+                <Route path='/Projects' element={<Projects/>}/>
+                <Route path='/Contact' element={<Contact/>}/>
+                 {/* <Route path="/" element={<Navigate replace to={Paths.HOME} />} /> */}
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,70 @@
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <Card style={{ maxWidth: 450, margin: "0 auto", padding: "20px 5px" }}>
+            <CardContent>
+                <Typography gutterBottom variant="h4">Contact Me</Typography>
+                <Typography gutterBottom color="textSecondary" variant="body2" component="p">Fill up the form and I will get back to you within 24 hours.</Typography>
+                <form>
+                    <Grid container spacing={1}>
+
+                        <Grid xs={12} sm={6} item>
+                            <TextField
+                                required
+                                label="First Name"
+                                placeholder='Enter first name'
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid xs={12} sm={6} item>
+                            <TextField
+                                required
+                                label="Last Name"
+                                placeholder='Enter last name'
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid xs={12} item>
+                            <TextField
+                                required
+                                type="email"
+                                label="Email"
+                                placeholder='Enter email'
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid xs={12} item>
+                            <TextField
+                                required
+                                type="number"
+                                label="Phone number"
+                                placeholder='Enter phone number'
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid xs={12} item>
+                            <TextField
+                                label="Message"
+                                placeholder='Type message here'
+                                multiline rows={4}
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid xs={12} item>
+                            <Button type='submit' variant="contained" color='primary' fullWidth>Submit</Button>
+                        </Grid>
+
+                    </Grid>
+
+                </form>
+
+            </CardContent>
+        </Card>)
+}
