f148b47a0a02bde901ed9ffbd6419fa6aad73dd8
kitanskitsvetelin <kitanskitsvetelin@gmail.com>
2023-04-26 15:06:51 +0300

    Update Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <h2>Home</h2>
-  )
+    <>
+      <div>Hi there👋, I'm</div>
+      <div className="name">Tsvetelin Kitanski</div>
+      <p>I try to make awesome websites</p>
+    </>
+  );
 }
