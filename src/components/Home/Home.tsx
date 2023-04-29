36c630eac2c4d03063c9e6d822b23f02be6bb6ba
kitanskitsvetelin <kitanskitsvetelin@gmail.com>
2023-04-29 09:18:23 +0300

    Home style update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Home.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="title-wrapper">
-      <div>Hi there👋, I'm</div>
-      <div className="name">Tsvetelin Kitanski</div>
-      <p className="description">I try to make awesome websites</p>
-    </div>
+    <>
+      <div className="title-wrapper">
+        <div>Hi there👋, I'm</div>
+        <div className="name">Tsvetelin Kitanski</div>
+        <p className="description">I try to make awesome websites</p>
+      </div>
+      <Button
+        sx={{ marginLeft: 10, marginTop: 5 }}
+        variant="outlined"
+        size="large"
+      >
+        Get in touch
+      </Button>
+    </>
   );
 }
