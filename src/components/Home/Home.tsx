13a4577461502fca27c366c062e6cee266be4a19
kitanskitsvetelin <kitanskitsvetelin@gmail.com>
2023-04-29 08:45:27 +0300

    update Home style

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,10 +3,10 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <>
+    <div className="title-wrapper">
       <div>Hi there👋, I'm</div>
       <div className="name">Tsvetelin Kitanski</div>
       <p>I try to make awesome websites</p>
-    </>
+    </div>
   );
 }
