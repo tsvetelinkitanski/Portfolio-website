a51afc791007d847713cfe87fc1adbf7539792e6
kitanskitsvetelin <kitanskitsvetelin@gmail.com>
2023-07-24 16:51:55 +0300

    change in css

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./About.css";
 
+
 export default function About() {
   return (
     <div id="about">
-      Hi there, I'm Tsvetelin Kitanski, a young intern web developer with a
-      passion for information technology. I'm excited about the endless
-      possibilities that the industry offers and eager to learn as much as I
-      can. My goal is to become a successful web developer and contribute to the
-      ever-growing digital landscape. I'm constantly seeking new challenges and
-      opportunities to expand my skills, so feel free to reach out and connect
-      with me!
+      <img className="picture" src={require('../../images/profilepicture.jpg')} alt="Profile picture" />
+
+      <div>
+        Hi there, I'm Tsvetelin Kitanski, a young intern web developer with a
+        passion for information technology. I'm excited about the endless
+        possibilities that the industry offers and eager to learn as much as I
+        can. My goal is to become a successful web developer and contribute to
+        the ever-growing digital landscape. I'm constantly seeking new
+        challenges and opportunities to expand my skills, so feel free to reach
+        out and connect with me!
+      </div>
+
     </div>
   );
 }
